362cd5f96496bb7ba06bad94d20784a2eb572242
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-06-24 16:05:16 +0300

    fix email verification

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -9,9 +9,13 @@ const signin = async (req, res) => {
   if (error) throw HttpError(400, error.message);
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   if (!user) throw HttpError(404);
   const comparePassword = await bcrypt.compare(password, user.password);
   if (!comparePassword) throw HttpError(401, 'Email or Password wrong');
+  if (!user.verify) {
+    throw HttpError(401, 'Email is not verified');
+  }
   const payload = {
     id: user._id,
   };
@@ -21,12 +25,12 @@ const signin = async (req, res) => {
   const newUser = await User.findByIdAndUpdate(user.id, { token });
 
   console.log(newUser);
-  const { token: userToken, ...userInfo } = newUser._doc;
+  const { token: prevToken, ...userInfo } = newUser._doc;
 
   res.json({
     message: 'Success Signed In',
     user: userInfo,
-    token: userToken,
+    token,
   });
 };
 
