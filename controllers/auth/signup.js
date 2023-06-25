9bb703a49ce57aa8db0501b8813a03b152b9e21c
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-06-25 21:11:06 +0300

    change email verification link

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,33 +1,33 @@
-const bcrypt = require("bcrypt");
-const { HttpError, sendEmail } = require("../../helpers");
-const { User, schemas } = require("../../models/authSchema");
-const { ctrlWrapper } = require("../../helpers");
-const {AvatarGenerator} = require ('random-avatar-generator');
-const { v4: uuidv4 } = require("uuid");
+const bcrypt = require('bcrypt');
+const { HttpError, sendEmail } = require('../../helpers');
+const { User, schemas } = require('../../models/authSchema');
+const { ctrlWrapper } = require('../../helpers');
+const { AvatarGenerator } = require('random-avatar-generator');
+const { v4: uuidv4 } = require('uuid');
 const generator = new AvatarGenerator();
 
 const APP_HOST = process.env.APP_HOST;
 
 const signup = async (req, res) => {
-	const { error } = schemas.userSchemaSignup.validate(req.body);
-	if (error) throw HttpError(400, error.message);
-	const { email, password } = req.body;
-	const user = await User.findOne({ email });
-	if (user) throw HttpError(409, "Email already in use");
-	const hashPassword = await bcrypt.hash(password, 10);
-	const avatar = await generator.generateRandomAvatar();
-	const verificationToken = uuidv4();
-	const newUser = await User.create({
-		...req.body,
-		password: hashPassword,
-		avatarURL: avatar,
-		verificationToken,
-	});
-    const verifyEmail = {
-		from: process.env.UKR_NET_EMAIL,
-		to: email,
-        subject: "Verify email",
-        html: `
+  const { error } = schemas.userSchemaSignup.validate(req.body);
+  if (error) throw HttpError(400, error.message);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) throw HttpError(409, 'Email already in use');
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatar = await generator.generateRandomAvatar();
+  const verificationToken = uuidv4();
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL: avatar,
+    verificationToken,
+  });
+  const verifyEmail = {
+    from: process.env.UKR_NET_EMAIL,
+    to: email,
+    subject: 'Verify email',
+    html: `
 		<div style="background-color: #f5f5f5; padding: 20px;">
 		  <div style="background-color: white; padding: 20px; border-radius: 5px;">
 			<h2 style="text-align: center; color: #333;">Verify Your Email Address</h2>
@@ -37,7 +37,11 @@ const signup = async (req, res) => {
 			<div style="text-align: center; margin-top: 30px;">
 			  <a href="${APP_HOST}/api/auth/verify/${newUser.verificationToken}"
 				style="display: inline-block; padding: 12px 24px; background-color: #4caf50; color: white; text-decoration: none; border-radius: 5px;">
-				Verify Email
+				Verify Email via Back-end (Temporary)
+			  </a>
+				<a href="http://localhost:3000/final-project-frontend-goose/register/${newUser.verificationToken}"
+				style="display: inline-block; padding: 12px 24px; background-color: #4caf50; color: white; text-decoration: none; border-radius: 5px;">
+				Verify Email via Front-end (Temporary on localhost:3000)
 			  </a>
 			</div>
 			<p style="text-align: center; color: #777; margin-top: 30px;">
@@ -46,13 +50,13 @@ const signup = async (req, res) => {
 		  </div>
 		</div>
 	  `,
-    };
-    await sendEmail(verifyEmail);
+  };
+  await sendEmail(verifyEmail);
 
-	res.json({
-		message: "User created successfully",
-		newUser
-	});
+  res.json({
+    message: 'User created successfully',
+    newUser,
+  });
 };
 
 module.exports = { signup: ctrlWrapper(signup) };
