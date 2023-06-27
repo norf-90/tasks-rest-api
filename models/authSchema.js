f03fff9414898aa099032ad080093f102a9b5d28
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-06-27 18:51:26 +0300

    change birthday validation

--- models/authSchema.js
+++ models/authSchema.js
@@ -39,7 +39,6 @@ const userSchema = new Schema(
     birthday: {
       type: String,
       default: '',
-      match: /^(0[1-9]|[12][0-9]|3[01]):(0[1-9]|1[0-2]):\d{4}$/, 
     },
     skype: {
       type: String,
