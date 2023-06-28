d012605c2de1bdf421ec88bef696f010ac28045d
tooreagen <tooreagen@gmail.com>
2023-06-28 23:04:46 +0300

    add getAllReviews function

--- controllers/reviews/getAllReviews.js
+++ controllers/reviews/getAllReviews.js
@@ -1,10 +1,23 @@
 const { Review } = require("../../models/reviewSchema");
+const { User } = require("../../models/authSchema");
 const { ctrlWrapper } = require("../../helpers");
 
 const getAllReviews = async (req, res) => {
-  const result = await Review.find({});
-  console.log(result);
-  res.json(result);
+  const resultReviews = await Review.find({});
+
+  const updatedReviews = await Promise.all(
+    resultReviews.map(async (item) => {
+      const resultUserAvatarUrl = await User.findOne(
+        { name: item.name },
+        "avatarURL"
+      );
+      if (resultUserAvatarUrl) {
+        item = { ...item._doc, avatarURL: resultUserAvatarUrl.avatarURL };
+      }
+      return item;
+    })
+  );
+  res.json(updatedReviews);
 };
 
 module.exports = {
