f008e6b3af4f5a44b961d08cf3d7344a76ed1935
tooreagen <tooreagen@gmail.com>
2023-06-29 19:18:30 +0300

    fix getUserReview: find by owner

--- controllers/reviews/getUserReview.js
+++ controllers/reviews/getUserReview.js
@@ -2,7 +2,10 @@ const { Review } = require("../../models/reviewSchema");
 const { ctrlWrapper, HttpError } = require("../../helpers");
 
 const getUserReview = async (req, res) => {
-  const resultReview = await Review.findOne({ name: req.user.name });
+  const { _id } = req.user;
+  const owner = _id.toString();
+
+  const resultReview = await Review.findOne({ owner: owner });
 
   if (!resultReview) {
     throw HttpError(404, "Reviews not found");
