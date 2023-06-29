b4e4c26d1fe5af18a0ac198c74f7703b576f31d6
tooreagen <tooreagen@gmail.com>
2023-06-29 17:56:29 +0300

    add addUserReview

--- controllers/reviews/addUserReview.js
+++ controllers/reviews/addUserReview.js
@@ -1,7 +1,16 @@
 const { Review } = require("../../models/reviewSchema");
-const { ctrlWrapper } = require("../../helpers");
+const { ctrlWrapper, HttpError } = require("../../helpers");
 
-const addUserReview = async (req, res) => {};
+const addUserReview = async (req, res) => {
+  const existingReview = await Review.findOne({ name: req.user.name });
+
+  if (existingReview) {
+    throw HttpError(404, "The review exists");
+  }
+  
+  const resultReview = await Review.create(req.body);
+  res.status(201).json(resultReview);
+};
 
 module.exports = {
   addUserReview: ctrlWrapper(addUserReview),
