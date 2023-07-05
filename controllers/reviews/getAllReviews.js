75d6d3e69e1e25b2836c27dc4a4e2885d25a0046
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-07-05 08:33:33 +0300

    delete commented code

--- controllers/reviews/getAllReviews.js
+++ controllers/reviews/getAllReviews.js
@@ -12,17 +12,6 @@ const getAllReviews = async (req, res) => {
     throw HttpError(404, 'Reviews not found');
   }
 
-  // const updatedReviews = await Promise.all(
-  //   resultReviews.map(async item => {
-  //     const { owner } = item;
-
-  //     const resultUserAvatarUrl = await User.findOne({ _id: owner }, 'avatarURL');
-  //     if (resultUserAvatarUrl) {
-  //       item = { ...item._doc, avatarURL: resultUserAvatarUrl.avatarURL };
-  //     }
-  //     return item;
-  //   })
-  // );
   res.json(reviews);
 };
 
